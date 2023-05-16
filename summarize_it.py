772d274335a8a08afcade325e08ac80e894b60bd
Borna Novak <dosadnizub@gmail.com>
2023-05-16 11:07:58 +0200

    more heat

--- summarize_it.py
+++ summarize_it.py
@@ -10,13 +10,13 @@ OPENAI_API_KEY=os.environ.get('OPENAI_API_KEY', 'INSERT HERE')
 loader = YoutubeLoader.from_youtube_url(sys.argv[1], add_video_info=True)
 result = loader.load()
 
-llm = OpenAI(temperature=0, openai_api_key=OPENAI_API_KEY)
+llm = OpenAI(temperature=0.5, openai_api_key=OPENAI_API_KEY)
 
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=2000, chunk_overlap=0)
 texts = text_splitter.split_documents(result)
 
-chain = load_summarize_chain(llm, chain_type="map_reduce", verbose=True)
-chain.run(texts[:4])
+chain = load_summarize_chain(llm, chain_type="map_reduce", verbose=False)
+print(chain.run(texts[:4]))
 
 
 
